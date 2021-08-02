4dde68c5b1fba172bed0f2815e3832152faa7005
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-02 11:18:40 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class ="text-center mt-4">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- NOTE v-for itterates over a collection where the first word is the variable to represent each element, in teh collection
+    :key is the property on that object that can be used to uniquely identify it -->
+    <div class='col-md-4' v-for="t in state.targets" :key="t.id">
+      <!-- //No longer state.target.name / health -->
+      <h1: v-if="t.health >1"> {(t.role)}</h3>
+      <h3: v-else> Unconsious</h3:>
+      <h1: class="{text-danger: "
+      <button :disabled="t.health < 1" class ="btn btn-primary" @click ="attack( 'slap', 1, t)">Slap </button>
+      <button :disabled="t.health < 1" class ="btn btn-primary" @click ="attack( 'kick', 2, t)">Kick </button>
+      <button :disabled="t.health < 1" class ="btn btn-primary" @click ="attack( 'punch', 3, t)">Punch </button>
+      <button v-if="t.health < 1" class ="btn btn-info" @click="t.health = t.maxHealth" >defib</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { reactive } from '@vue/reactivity'
+import { computed } from '@vue/runtime-core'
+export default {
+  name: 'App',
+  setup(){
+    // NOTE reactive creates object where all properties are subscriped
+    const state = reactive({
+      targets: [ {
+        id: 1,
+        role: 'Instructor',
+        name: 'Mark',
+        health: 100,
+        maxHealth: 100,
+        dead: computed(()=> this.health < 1)
+        },
+        {
+        id: 2,
+        role: 'Instrucotr',
+        name: 'Jake',
+        health: 120,
+        maxHealth: 120,
+        dead: computed(()=> this.health < 1)
+        }, {
+        id: 3,
+        role: 'TA',
+        name: 'Justin',
+        health: 80,
+        maxHealth: 80,
+        dead: computed(()=> this.health < 1)
+        }
+        ]
+    })
+   // const target = {
+    //   role: 'Instructor',
+    //   name: 'Mark',
+    //   health: 100 not in state
+    return {
+      attack(descr, val, target){
+        console.log('')
+        target.health -= val
+        if(target.health < 0) {
+          target.health = 0
+        }
+        // draw()
+      },
+      state
+      //target moved
+    }
+  }
+}
+</script>
+
+<style scoped>
+.strikethorugh{
+  text-decoration: line-through;
+}
+</style>
