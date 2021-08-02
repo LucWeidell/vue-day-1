928cfa9256fc7a4e6c73e3f3c53e8f8860bcc780
W1ndyWalker <60443786+W1ndyWalker@users.noreply.github.com>
2021-08-02 12:25:40 -0600

    everything works but buttons not displaying styled

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,34 @@
 <template>
-  <div class ="text-center mt-4">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <form @submit.prevent="addTarget">
+  <div class="text-center mt-4 container">
+    <div class="row">
+      <div class="col-12">
+          <img alt="Vue logo" src="./assets/logo.png">
+          <form @submit.prevent="addTarget">
 
-        <!--  NOTE two way data binding with a v-model  -->
-      <input type="text" placeholder="name" v-model="state.newTarget.name">
-      <input type="text" placeholder="role" v-model="state.newTarget.role">
-      <input type="text" placeholder="health" v-model="state.newTarget.health">
-      <button type="submit" class="btn btn-primary">Add</button>
-    </form>
-    <!-- NOTE v-for itterates over a collection where the first word is the variable to represent each element, in teh collection
-    :key is the property on that object that can be used to uniquely identify it -->
-    <div class='col-md-4' v-for="t in state.targets" :key="t.id">
+              <!--  NOTE two way data binding with a v-model  -->
+            <input type="text" placeholder="name" v-model="state.newTarget.name">
+            <input type="text" placeholder="role" v-model="state.newTarget.role">
+            <input type="text" placeholder="health" v-model="state.newTarget.health">
+            <button type="submit" class="btn btn-primary">Add</button>
+          </form>
+        </div>
+      <!-- NOTE v-for itterates over a collection where the first word is the variable to represent each element, in teh collection
+      :key is the property on that object that can be used to uniquely identify it -->
+    <div class="col-md-4" v-for="t in state.targets" :key="t.id">
       <!-- //No longer state.target.name / health -->
-      <h1 v-if="t.health < 1">{{t.role}}</h1>
-      <h3 v-else> Unconsious</h3>
-      <!-- FIXME  -->
-      <h1 :class="{'text-danger': t.health < 1, strickthrough: t.health < 10 }"> {{ t.name }} - {{t.health}} <h1>
-      <button :disabled="t.health < 1" class ="btn btn-primary" @click ="attack( 'slap', 1, t)">Slap </button>
-      <button :disabled="t.health < 1" class ="btn btn-warning" @click ="attack( 'kick', 2, t)">Kick </button>
-      <button :disabled="t.health < 1" class ="btn btn-danger" @click ="attack( 'punch', 3, t)">Punch </button>
+      <h1 v-if="t.health > 1">{{t.role}}</h1>
+      <h3 v-else>Unconsious</h3>
+      <h1 :class="{ 'text-danger': t.health < 1, strickthrough: t.health < 10 }"> {{ t.name }} - {{t.health}} </h1>
+      <button :disabled="t.health < 1" class="btn btn-primary" @click ="attack( 'slap', 1, t)">Slap </button>
+      <button :disabled="t.health < 1" class="btn btn-warning" @click ="attack( 'kick', 2, t)">Kick </button>
+      <button :disabled="t.health < 1" class="btn btn-danger" @click ="attack( 'punch', 3, t)">Punch </button>
 
       <button v-show="t.health < 1" class ="btn btn-info" @click="t.health = t.maxHealth" >defib</button>
       <!--NOTE cool if
-       <button v-if="t.health < 1" class ="btn btn-info" @click="t.health = t.maxHealth" >defib</button> -->
+      <button v-if="t.health < 1" class ="btn btn-info" @click="t.health = t.maxHealth" >defib</button> -->
     </div>
-    <h1 v-if="state.allDead">All Targets Eliminated You are now the Owner of CodeWorks
+    <h1 v-if="state.allDead">All Targets Eliminated You are now the Owner of CodeWorks</h1>
+  </div>
   </div>
 </template>
 
@@ -81,17 +84,17 @@ export default {
     //   health: 100 not in state
     return {
       attack(descr, val, target){
-        console.log('')
+        console.log(descr)
         target.health -= val
         if(target.health < 0) {
           target.health = 0
-        }--
+        }
         // draw()
       },
       addTarget(){
-        this.newTarget.maxHealth = state.target.health
-        state.target.push(state.newTarget)
-        this.newTarget = {}
+        state.newTarget.maxHealth = state.targets.health
+        state.targets.push(state.newTarget)
+        state.newTarget = {}
       },
       state
       //target moved
